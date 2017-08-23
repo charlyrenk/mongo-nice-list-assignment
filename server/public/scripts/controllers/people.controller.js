edce77712971114a08a0dbd4d4ba2816a106a501
Kris Szafranski <kdszafranski@gmail.com>
2017-08-23 14:12:26 -0500

    rewind to lecture start

--- server/public/scripts/controllers/people.controller.js
+++ server/public/scripts/controllers/people.controller.js
@@ -2,40 +2,5 @@ myApp.controller('PeopleController', ['$http', function($http) {
     var vm = this;
     vm.newPerson = {};
 
-    // get the people data from the server and fill the DOM
-    getPeople();
-
-    vm.addPerson = function() {
-      console.log('add person', vm.newPerson);
-      $http.post('/person', vm.newPerson)
-        .then(function(response) {
-          console.log('added person', response);
-          getPeople();
-        });
-    }
-
-    vm.updatePerson = function(id) {
-      console.log('update person with id: ', id);
-      var data = {location: 'Idaho'};
-      $http.put('/person/' + id, data)
-        .then(function(response) {
-          getPeople();
-        });
-    }
-
-    vm.deletePerson = function(id) {
-      console.log('delete person with id: ', id);
-      $http.delete('/person/' + id)
-        .then(function(response) {
-          getPeople();
-        })
-    }
-
-    function getPeople() {
-      $http.get('/person').then(function(response) {
-        console.log(response.data);
-        vm.people = response.data;
-      });
-    }
 
 }]);
--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var index = require('./routes/index.js');
-var person = require('./routes/person.js');
 var mongoose = require('mongoose');
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -11,22 +10,7 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use(bodyParser.json()); // needed for angular requests
 
 /** ---------- EXPRESS ROUTES ---------- **/
-app.use('/person', person);
 app.use('/', index);
-
-
-/** -------- MONGOOSE CONNECTION --------**/
-var databaseUrl = 'mongodb://localhost:27017/antares';
-mongoose.connect(databaseUrl);
-
-mongoose.connection.on('connected', function() {
-  console.log('mongoose connected to : ', databaseUrl);
-});
-
-mongoose.connection.on('error', function(err) {
-  console.log('mongoose connection error: ', err);
-});
-
 
 
 /** ---------- START SERVER ---------- **/
